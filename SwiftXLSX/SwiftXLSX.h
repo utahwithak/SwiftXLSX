86cb3a405e78837ba0698843a17f79233291c51f
Carl Wieland <cwieland@esri.com>
2017-04-28 12:30:44 -0400

    Initial Commit

---
+++ SwiftXLSX/SwiftXLSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftXLSX.h
+//  SwiftXLSX
+//
+//  Created by Carl Wieland on 4/28/17.
+//  Copyright © 2017 Datum. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftXLSX.
+FOUNDATION_EXPORT double SwiftXLSXVersionNumber;
+
+//! Project version string for SwiftXLSX.
+FOUNDATION_EXPORT const unsigned char SwiftXLSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftXLSX/PublicHeader.h>
+
+
---
+++ SwiftXLSXTests/SwiftXLSXTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftXLSXTests.swift
+//  SwiftXLSXTests
+//
+//  Created by Carl Wieland on 4/28/17.
+//  Copyright © 2017 Datum. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftXLSX
+
+class SwiftXLSXTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
